a2e88a6414c4b29e74b0319cd4be066dedb668be
Matt Wood <matt@Matts-16-M1.local>
2023-09-11 17:36:48 -0600

    finished lab1

--- Lab1/print_requests_version.py
+++ Lab1/print_requests_version.py
@@ -1,2 +1,9 @@
+#To activate venv: source venv/bin/activatepython
+
+#References: Used ChatGPT for basic syntax
+
 import requests
-print(requests.__version__)
+
+site = 'https://raw.githubusercontent.com/woodepic/CMPUT_404_Labs/main/Lab1/print_requests_version.py'
+response = requests.get(site)
+print(response.text)
