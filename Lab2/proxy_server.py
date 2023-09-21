f9054fb9b29129f92b02f0998b35b17fe4eed89f
Matt Wood <matt@Matts-16-M1.local>
2023-09-21 17:59:05 -0600

    finished proxy server

--- Lab2/proxy_server.py
+++ Lab2/proxy_server.py
@@ -1,17 +1,40 @@
 import socket
 
-# Define the target host and port
-target_host = "www.google.com"
-target_port = 80  # Port 80 is the default HTTP port
+google_addr = "www.google.com"
 
-# Create a socket object (to  connec to google)
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET indicated IPV4, and SOCK_STREAM indicates a TCP connection
+#Create the socket used to talk to the client
+proxy_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+proxy_server_address = ('localhost', 8001)
+proxy_server_socket.bind(proxy_server_address)
+proxy_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+proxy_server_socket.listen()
+print("Proxy server is listening on port 8001...")
 
-# Connect to the server
-client_socket.connect((target_host, target_port))
+while True:
+    # Wait for a connection
+    print("Waiting for a connection...")
+    client_socket, client_address = proxy_server_socket.accept()
+    print(f"Accepted client connection from {client_address}")
 
-#receive text from client
-#echo text to google
-#Receive response from google
-#echo response to client
-#repeat
+    # Create a socket object and connect to google
+    google_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET indicated IPV4, and SOCK_STREAM indicates a TCP connection
+    google_socket.connect((google_addr, 80))
+    print(f"Connected client to {google_addr}...")
+
+
+    #Echo text from client to google
+    #Echo response from google to client
+    while True:
+        data = client_socket.recv(4096)
+        if data:
+            google_socket.send(data)
+            google_socket.shutdown(socket.SHUT_WR)
+            response = google_socket.recv(4096)
+            client_socket.send(response)
+
+        else:
+            # If no data is received, close the connection
+            print("No more data received. Closing connection.\n\n")
+            client_socket.close()
+            google_socket.close()
+            break
