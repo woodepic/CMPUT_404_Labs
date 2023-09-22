c002403eade020cc076b8181ff7dd6fcfc110d85
Matt Wood <matt@Matts-16-M1.local>
2023-09-21 18:24:03 -0600

    modified to contain single threaded start command

--- Lab2/echo_server.py
+++ Lab2/echo_server.py
@@ -7,30 +7,8 @@
 
 import socket
 
-
-
-server_host = "localhost"
-server_port = 8001
-
-# Create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to a specific address and port
-server_address = ('localhost', 8001)
-server_socket.bind(server_address)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# Listen for incoming connections
-server_socket.listen()
-print("Server is listening on port 8001...")
-
-
-while True:
-    # Wait for a connection
-    print("Waiting for a connection...")
-    client_socket, client_address = server_socket.accept()
+def handle_connection(client_socket, client_address):
     print(f"Accepted connection from {client_address}")
-
     while True:
         # Receive data from the client
         data = client_socket.recv(1024)
@@ -45,3 +23,28 @@ while True:
             print("No more data received. Closing connection.\n\n")
             client_socket.close()
             break
+
+def start_server_st():
+    server_host = "localhost"
+    server_port = 8001
+
+    # Create a socket object
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to a specific address and port
+    server_address = ('localhost', 8001)
+    server_socket.bind(server_address)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Listen for incoming connections
+    server_socket.listen()
+    print("Server is listening on port 8001...")
+
+    while True:
+        # Wait for a connection
+        print("Waiting for a connection...")
+        client_socket, client_address = server_socket.accept()
+        handle_connection(client_socket, client_address)
+
+if __name__ == "__main__":
+    start_server_st()
